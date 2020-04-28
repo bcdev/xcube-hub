b04152be32c5699eaaf61270721e0642d84c1005
helge <lmn9p5zktm&Ptu>
2020-04-28 09:11:28 +0200

    removed some files that do not belong into this PR

--- xcube_gen/wsgi.py
+++
@@ -1,9 +0,0 @@
-from .service import new_app
-
-app = new_app()
-
-if __name__ == "__main__":
-    # Only for debugging while developing
-    app.run(host='0.0.0.0', port=80, debug=True)
-else:
-    application = app
