dc00547a2b470dbe6314d722dcb3073e8e2d14c1
helge <lmn9p5zktm&Ptu>
2020-10-22 10:02:59 +0200

    testing timeout on travis

--- test/controllers/test_jobs.py
+++ test/controllers/test_jobs.py
@@ -8,52 +8,52 @@ import subprocess
 
 
 # @unittest.skipIf(os.environ.get("UNITTEST_WITH_K8S", False) == 'true', "K8s test supressed.")
-# class TestJobs(unittest.TestCase):
-#     def setUp(self) -> None:
-#         os.environ["RUN_LOCAL"] = '1'
-#         os.environ["XCUBE_GEN_API_CALLBACK_URL"] = 'http://test/'
-#         self._access_token = setup_auth()
-#         self._app = new_app()
-#         self._client = self._app.test_client()
-#         self._client.environ_base['HTTP_AUTHORIZATION'] = 'Bearer ' + self._access_token['access_token']
-#         subprocess.call(["kubectl", "create", "namespace", "daffy-duck"])
-#
-#     def tearDown(self) -> None:
-#         subprocess.call(["kubectl", "delete", "namespace", "daffy-duck"])
-#
-#     def test_create(self):
-#         res = self._client.put('/jobs/daffy-duck', json=SH_CFG)
-#         self.assertEqual("200 OK", res.status)
-#
-#     def test_create_without_callback_url(self):
-#         del os.environ["XCUBE_GEN_API_CALLBACK_URL"]
-#         res = self._client.put('/jobs/daffy-duck', json=SH_CFG)
-#
-#         response = res.json
-#
-#         self.assertEqual(400, res.status_code)
-#         self.assertEqual('XCUBE_GEN_API_CALLBACK_URL must be given', response['message'])
-#
-#     def test_delete(self):
-#         set_env()
-#         res = self._client.delete('/jobs/daffy-duck/a-test')
-#         self.assertEqual('404 NOT FOUND', res.status)
-#
-#     def test_get(self):
-#         set_env()
-#         res = self._client.get('/jobs/daffy-duck/a-test')
-#         self.assertEqual('404 NOT FOUND', res.status)
-#
-#     def test_list(self):
-#         set_env()
-#         res = self._client.get('/jobs/daffy-duck')
-#         self.assertEqual('200 OK', res.status)
-#
-#     def test_delete_all(self):
-#         set_env()
-#         res = self._client.delete('/jobs/daffy-duck')
-#         self.assertEqual('200 OK', res.status)
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
+class TestJobs(unittest.TestCase):
+    def setUp(self) -> None:
+        os.environ["RUN_LOCAL"] = '1'
+        os.environ["XCUBE_GEN_API_CALLBACK_URL"] = 'http://test/'
+        self._access_token = setup_auth()
+        self._app = new_app()
+        self._client = self._app.test_client()
+        self._client.environ_base['HTTP_AUTHORIZATION'] = 'Bearer ' + self._access_token['access_token']
+        subprocess.call(["kubectl", "create", "namespace", "daffy-duck"])
+
+    def tearDown(self) -> None:
+        subprocess.call(["kubectl", "delete", "namespace", "daffy-duck"])
+
+    def test_create(self):
+        res = self._client.put('/jobs/daffy-duck', json=SH_CFG)
+        self.assertEqual("200 OK", res.status)
+
+    def test_create_without_callback_url(self):
+        del os.environ["XCUBE_GEN_API_CALLBACK_URL"]
+        res = self._client.put('/jobs/daffy-duck', json=SH_CFG)
+
+        response = res.json
+
+        self.assertEqual(400, res.status_code)
+        self.assertEqual('XCUBE_GEN_API_CALLBACK_URL must be given', response['message'])
+
+    def test_delete(self):
+        set_env()
+        res = self._client.delete('/jobs/daffy-duck/a-test')
+        self.assertEqual('404 NOT FOUND', res.status)
+
+    def test_get(self):
+        set_env()
+        res = self._client.get('/jobs/daffy-duck/a-test')
+        self.assertEqual('404 NOT FOUND', res.status)
+
+    def test_list(self):
+        set_env()
+        res = self._client.get('/jobs/daffy-duck')
+        self.assertEqual('200 OK', res.status)
+
+    def test_delete_all(self):
+        set_env()
+        res = self._client.delete('/jobs/daffy-duck')
+        self.assertEqual('200 OK', res.status)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- test/controllers/test_users.py
+++ test/controllers/test_users.py
@@ -12,49 +12,49 @@ from xcube_hub.controllers.users import subtract_processing_units
 from xcube_hub.database import DEFAULT_DB_BUCKET_NAME
 from xcube_hub.service import new_app
 
-#
-# class UsersTest(unittest.TestCase):
-#     def setUp(self) -> None:
-#         os.environ["RUN_LOCAL"] = '1'
-#         self._access_token = setup_auth()
-#         self._app = new_app()
-#         self._client = self._app.test_client()
-#         self._client.environ_base['HTTP_AUTHORIZATION'] = 'Bearer ' + self._access_token['access_token']
-#
-#     def test_update_processing_units(self):
-#         with moto.mock_s3():
-#             s3 = boto3.client('s3')
-#             s3.create_bucket(Bucket=DEFAULT_DB_BUCKET_NAME)
-#
-#             user_name = 'heinrich'
-#             user_id = hashlib.md5(user_name.encode('utf-8')).hexdigest()
-#
-#             punits_request_1 = dict(punits=dict(total_count=5000,
-#                                     user_name=user_name,
-#                                     price_amount=200,
-#                                     price_currency='€'))
-#             punits_request_2 = dict(punits=dict(total_count=10000,
-#                                     user_name=user_name,
-#                                     price_amount=350,
-#                                     price_currency='€'))
-#             punits_request_3 = dict(punits=dict(total_count=280,
-#                                     user_name=user_name))
-#
-#             add_processing_units(user_id, punits_request_1)
-#
-#             add_processing_units(user_id, punits_request_2)
-#
-#             subtract_processing_units(user_id, punits_request_3)
-#
-#             punits_data = get_user_data(user_id, 'punits')
-#             self.assertIsNotNone(punits_data)
-#             self.assertIn('count', punits_data)
-#             self.assertIn('history', punits_data)
-#             actual_count = punits_data.get('count')
-#             actual_history = punits_data.get('history')
-#             self.assertEqual(5000 + 10000 - 280, actual_count)
-#             self.assertIsInstance(actual_history, list)
-#             self.assertEqual(3, len(actual_history))
-#             self.assertEqual(['sub', punits_request_3], actual_history[0][1:])
-#             self.assertEqual(['add', punits_request_2], actual_history[1][1:])
-#             self.assertEqual(['add', punits_request_1], actual_history[2][1:])
+
+class UsersTest(unittest.TestCase):
+    def setUp(self) -> None:
+        os.environ["RUN_LOCAL"] = '1'
+        self._access_token = setup_auth()
+        self._app = new_app()
+        self._client = self._app.test_client()
+        self._client.environ_base['HTTP_AUTHORIZATION'] = 'Bearer ' + self._access_token['access_token']
+
+    def test_update_processing_units(self):
+        with moto.mock_s3():
+            s3 = boto3.client('s3')
+            s3.create_bucket(Bucket=DEFAULT_DB_BUCKET_NAME)
+
+            user_name = 'heinrich'
+            user_id = hashlib.md5(user_name.encode('utf-8')).hexdigest()
+
+            punits_request_1 = dict(punits=dict(total_count=5000,
+                                    user_name=user_name,
+                                    price_amount=200,
+                                    price_currency='€'))
+            punits_request_2 = dict(punits=dict(total_count=10000,
+                                    user_name=user_name,
+                                    price_amount=350,
+                                    price_currency='€'))
+            punits_request_3 = dict(punits=dict(total_count=280,
+                                    user_name=user_name))
+
+            add_processing_units(user_id, punits_request_1)
+
+            add_processing_units(user_id, punits_request_2)
+
+            subtract_processing_units(user_id, punits_request_3)
+
+            punits_data = get_user_data(user_id, 'punits')
+            self.assertIsNotNone(punits_data)
+            self.assertIn('count', punits_data)
+            self.assertIn('history', punits_data)
+            actual_count = punits_data.get('count')
+            actual_history = punits_data.get('history')
+            self.assertEqual(5000 + 10000 - 280, actual_count)
+            self.assertIsInstance(actual_history, list)
+            self.assertEqual(3, len(actual_history))
+            self.assertEqual(['sub', punits_request_3], actual_history[0][1:])
+            self.assertEqual(['add', punits_request_2], actual_history[1][1:])
+            self.assertEqual(['add', punits_request_1], actual_history[2][1:])
