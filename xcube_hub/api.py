a107b69563ddc8433a7bb61c8006ba2d7044d411
dzelge <helge.dzierzon@brockmann-consult.de>
2021-03-04 18:19:15 +0100

    Fixed rsult return

--- xcube_hub/api.py
+++ xcube_hub/api.py
@@ -33,14 +33,14 @@ class ApiResponse:
 
     @classmethod
     def success(cls, result: Optional[Any] = None, message: Optional[str] = None) -> AnyDict:
-        response = dict()
-        if result is not None:
-            response['result'] = result
-
-        if message is not None:
-            response['message'] = message
-
-        return response
+        # response = dict()
+        # if result is not None:
+        #     response['result'] = result
+        #
+        # if message is not None:
+        #     response['message'] = message
+        #
+        return result
 
     @classmethod
     def error(cls,
