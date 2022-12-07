c1f763f9472a214e4c9fc00e274576090f4d2a6f
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2022-12-07 08:57:08 +0100

    completely remove namespace check

--- xcube_hub/core/cate.py
+++ xcube_hub/core/cate.py
@@ -31,11 +31,9 @@ from xcube_hub.typedefs import JsonObject
 from xcube_hub.util import maybe_raise_for_invalid_username
 
 
-def delete_cate(user_id: str, prune: bool = False, check_namespace=True) -> bool:
+def delete_cate(user_id: str, prune: bool = False) -> bool:
     cate_namespace = util.maybe_raise_for_env("WORKSPACE_NAMESPACE",
                                               default="cate")
-    if check_namespace:
-        user_namespaces.create_if_not_exists(user_namespace=cate_namespace)
 
     deployment = k8s.get_deployment(name=user_id + '-cate', namespace=cate_namespace)
 
@@ -233,7 +231,7 @@ def launch_cate(user_id: str) -> JsonObject:
                                                   lifecycle=lifecycle)
 
         # delete previous cate deployment to make sure pod is not restarted
-        delete_cate(user_id, check_namespace=False)
+        delete_cate(user_id)
 
         while k8s.get_deployment(name=user_id + '-cate', namespace=cate_namespace):
             # do not create the new deployment while the old one is still there
